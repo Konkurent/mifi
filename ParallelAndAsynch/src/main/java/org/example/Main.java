de9bf06daae9bddebb5b34be39b80dcbc51f9b6c
JavaMan <Kechin.d@gmail.com>
2025-06-02 11:35:20 +0300

    init

--- ParallelAndAsynch/src/main/java/org/example/Main.java
+++ ParallelAndAsynch/src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ import java.util.concurrent.Future;
 
 public class Main {
 
+
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         numbers.parallelStream()
